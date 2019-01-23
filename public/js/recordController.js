120354bb747a6c94104b73e712f662d1d4dd30f0
Aneesha <aneesha.n@way2online.co.in>
2019-01-24 02:09:36 +0530

    upto saving transcripts to db

--- public/js/recordController.js
+++ public/js/recordController.js
@@ -1,6 +1,9 @@
 /**
  * Created by sys2011 on 22/1/19.
  */
+// curl request
+// curl -X POST -u "apikey:TJfuoXFauZTSMOefqWkK40X339GcIdn4YoWlSB4520Bz" --header "Content-Type: audio/flac" --data-binary @audio-file.flac "https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize"
+
 var speechtotext = angular.module('speechtotext', []);
 speechtotext.controller('recordController', ['$scope', '$http', function ($scope, $http) {
     $scope.disabled = true;
@@ -66,9 +69,56 @@ speechtotext.controller('recordController', ['$scope', '$http', function ($scope
             mediaRecorder.stop();
             console.log(mediaRecorder.state);
             mediaRecorder.onstop = function (ev) {
-                var blob = new Blob(chunks, {'type': 'audio/mp4;'});
+                var blob = new Blob(chunks, {'type': 'audio/mp4'});
                 chunks = [];
                 var audioURL = window.URL.createObjectURL(blob);
+
+                // var blob = new Blob(chunks, {type: 'application/octet-stream'});
+                // saveAs(blob, "example.mp4");
+
+                // var fd = new FormData();
+                // fd.append('fname', 'audio.flac');
+                // fd.append('audioBlob', blob);
+                // $.ajax({
+                //     type: 'POST',
+                //     url: '/saveAudio',
+                //     data: fd,
+                //     processData: false,
+                //     contentType: false,
+                //     success: function (data) {
+                //         console.log("first");
+                //         $http({
+                //             method: "GET",
+                //             url: "/convertAudio"
+                //         }).then(function (response) {
+                //             console.log(response);
+                //         }, function (err) {
+                //             console.log(err);
+                //         });
+                //     },
+                //     xhr: function () {
+                //         var xhr = new XMLHttpRequest();
+                //         // listen to the 'progress' event
+                //         xhr.upload.addEventListener('progress', function (evt) {
+                //             console.log("second");
+                //         }, false);
+                //         return xhr;
+                //     }
+                // });
+
+                $http({
+                    method: "GET",
+                    url: "/convertAudio"
+                }).then(function (response) {
+                    if (response.data.result == 0) {
+                        $scope.errormessage = response.data.msg;
+                    }
+                    else {
+                        console.log("Success");
+                    }
+                }, function (err) {
+                    console.log(err);
+                });
             };
             $scope.record = false;
         }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,65 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-var save = require('save-file/sync');
+var formidable = require('formidable');
+var fs = require('fs');
+var exec = require('child_process').exec;
+var mongoClient = require('mongodb').MongoClient;
+var remoteMongoUrl = "mongodb://localhost:27017/truepush";
+var textCollection;
 
-/* GET home page. */
+function saveToDb(text, callback) {
+    if (textCollection != null) {
+        textCollection.insert({'_id': text}, function (err, insRes) {
+            if (err) {
+                if (err.code === 11000) {
+                    console.log('Duplicate text');
+                    callback(2);
+                }
+                else {
+                    console.log(err);
+                    callback(0);
+                }
+            }
+            else {
+                console.log('insRes:', insRes);
+                if(insRes.insertedCount == 1){
+                    callback(1);
+                }
+                else{
+                    callback(0);
+                }
+            }
+        })
+    }
+    else {
+        callback(0);
+    }
+}
+
+function getMongo() {
+    mongoClient.connect(remoteMongoUrl, function (err, db) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (db !== null) {
+                db.authenticate("user", "user", function (err, res) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log("Mongo connection established!");
+                        textCollection = db.collection('textCollection');
+                    }
+                });
+            }
+        }
+    });
+}
+
+getMongo();
+
 router.get('/', function (req, res, next) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.write("Hi");
@@ -15,8 +71,101 @@ router.get('/record', function (req, res, next) {
 });
 
 router.post('/saveAudio', function (req, res, next) {
-    console.log("req.body::", req.body);
-    res.send("Hii");
+    try {
+        var fid;
+        var form = new formidable.IncomingForm();
+        form.encoding = 'utf-8';
+        form.multiples = true;
+        form.uploadDir = path.join(__dirname, "../public/audio/");
+
+        // rename it to it's orignal name
+        form.on('field', function (field, value) {
+            // console.log("field",field, "value",value)
+            fid = value;
+        });
+        form.on('file', function (field, file) {
+            var file_name = fid.replace(/\s/g, '');
+
+            fs.rename(file.path, path.join(form.uploadDir, file_name));
+            // process.exit()
+        });
+        form.on('error', function (err) {
+            console.log('An error has occured: \n' + err);
+        });
+
+        // once all the files have been uploaded, send a response to the client
+        form.on('end', function () {
+            res.end('success');
+        });
+        // parse the incoming request containing the form data
+        form.parse(req);
+    }
+    catch (errr) {
+        console.error(errr.stack);
+    }
+});
+router.get('/convertAudio', function (req, res, next) {
+    var filepath = path.join(__dirname, '../public/audio/');
+    var command = 'curl -X POST -u "apikey:TJfuoXFauZTSMOefqWkK40X339GcIdn4YoWlSB4520Bz" --header "Content-Type: audio/flac" --data-binary @' + filepath + 'audio.flac "https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize"';
+    console.log(command);
+    try {
+        exec(command, function (error, stdout, stderr) {
+            if (error) {
+                console.log(error);
+                res.send({result: 0, msg: error.toString()});
+            }
+            // else if (stderr) {
+            //     console.log(stderr);
+            //     res.send({result: 0, msg: stderr.toString()});
+            // }
+            else if (stdout != '') {
+                // console.log("stdout:", stdout);
+
+                if (stdout.code == 400) {
+                    res.send({result: 1, msg: "Stream was 0 bytes but needs to be at least 100 bytes."});
+                }
+                else {
+                    if (JSON.parse(stdout).results != undefined) {
+                        if (JSON.parse(stdout).results[0].alternatives != undefined) {
+                            if (JSON.parse(stdout).results[0].alternatives[0].transcript != "") {
+                                // res.send({result: 1, msg: stdout.results[0].alternatives[0].transcript});
+                                console.log(JSON.parse(stdout).results[0].alternatives[0].transcript);
+                                saveToDb(JSON.parse(stdout).results[0].alternatives[0].transcript, function (resp) {
+                                    var msg;
+                                    if (resp === 0) {
+                                        msg = "Error while inserting data into Db.";
+                                    }
+                                    else if (resp === 1) {
+                                        msg = "Inserted the transcript into Db.";
+                                    }
+                                    else if (resp === 2) {
+                                        msg = "Transcript already exists in Db."
+                                    }
+                                    else {
+                                        msg = "Unknown error."
+                                    }
+                                    res.send({result: 1, msg: msg});
+                                });
+                            }
+                            else {
+                                res.send({result: 1, msg: "Empty transcript."});
+                            }
+                        }
+                        else {
+                            res.send({result: 1, msg: "No alternative transcript found."});
+                        }
+                    }
+                    else {
+                        res.send({result: 1, msg: "No results found."});
+                    }
+                }
+            }
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.send({result: 0});
+    }
 });
 
 module.exports = router;
